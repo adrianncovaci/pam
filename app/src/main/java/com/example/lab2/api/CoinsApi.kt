d055b3b02de159c76420a26c37cdad7988640bdd
Adrian Covaci <adrian.covaci@amdaris.com>
2021-03-11 22:28:30 +0200

    Added Lab 1

--- app/src/main/java/com/example/lab2/api/CoinsApi.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab2.api
-
-data class CoinsApi(
-    val id: String,
-    val name: String,
-    val symbol: String
-)
--- app/src/main/java/com/example/lab2/feed/models/FeedItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab2.feed.models
-
-class FeedItem(
-    val header: String,
-    val name: String,
-    val symbol: String
-)
